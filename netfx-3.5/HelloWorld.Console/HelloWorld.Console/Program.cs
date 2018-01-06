b5823e5439e945e33037211e955a270fe2d8d230
Elton Stoneman <elton@sixeyed.com>
2018-01-06 23:03:31 +0000

    Added .NET 3.5 console app

---
+++ netfx-3.5/HelloWorld.Console/HelloWorld.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace HelloWorld.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = new List<string> { "hello", "world" };
+            WriteLine($"{values[0]} {values[1]}");
+        }
+    }
+}
