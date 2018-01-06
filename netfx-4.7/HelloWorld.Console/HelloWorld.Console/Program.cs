234ab2582542f82b99ce0826b10109decf844da5
Elton Stoneman <elton@sixeyed.com>
2018-01-06 22:22:15 +0000

    Added 4.7 console app

---
+++ netfx-4.7/HelloWorld.Console/HelloWorld.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace HelloWorld.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use C# 7 feature:
+            var valueTuple = ("hello", "world");
+            WriteLine($"{valueTuple.Item1} {valueTuple.Item2}");
+        }
+    }
+}
