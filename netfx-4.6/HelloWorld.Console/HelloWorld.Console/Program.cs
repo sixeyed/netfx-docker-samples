7a43e667c86808874894ae6e3fd6326e3e68526d
Elton Stoneman <elton@sixeyed.com>
2018-01-06 22:53:01 +0000

    Added 4.6 Hello World console

---
+++ netfx-4.6/HelloWorld.Console/HelloWorld.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace HelloWorld.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // use .NET 4.6 feature:
+            var values = new Stack<string>();
+            values.Push("world");
+            values.Push(" ");
+            values.Push("hello");   
+            
+            foreach(var value in values as IReadOnlyCollection<string>)
+            {
+                Write(value);
+            }
+            WriteLine();
+        }
+    }
+}
